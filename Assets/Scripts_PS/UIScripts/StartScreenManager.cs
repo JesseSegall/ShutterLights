7aed288f453abbc2c21e6544c0e6f2ffbdf3ff35
Sandoval, Patrick Humberto <psandoval7@gatech.edu>
2025-03-08 19:50:13 -0500

    Created pause menu
    Created pause menu and buttons. Scripts have been moved to ps scripts. Additional scene in name to merge conflicts

---
+++ Assets/Scripts_PS/UIScripts/StartScreenManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartScreenManager : MonoBehaviour
+{
+    public AudioSource startSound; // Assign this in the Inspector
+
+    public void StartGame()
+    {
+        if (startSound != null)
+        {
+            StartCoroutine(PlaySoundAndLoadScene()); // Start coroutine to wait for sound
+        }
+        else
+        {
+            SceneManager.LoadScene("Room_1_psandvoal"); // Load immediately if no sound
+        }
+    }
+
+    IEnumerator PlaySoundAndLoadScene()
+    {
+        startSound.Play(); // Play the sound effect
+        yield return new WaitForSeconds(startSound.clip.length); // Wait for the sound to finish
+        SceneManager.LoadScene("Room_1_psandvoal"); // Load the next scene
+    }
+}
