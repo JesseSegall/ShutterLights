6ec9936e5d3f910ee6c25c01c825b30200a99dd7
Bradley Hammond <bhammond33@gatech.edu>
2025-03-09 08:01:45 -0400

    boss added

---
+++ Assets/ScriptsBH/ProjectileBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.CompareTag("Terrain"))
+    {
+        Destroy(gameObject);
+    }
+    if (collision.gameObject.CompareTag("Player")) 
+        {
+            Debug.Log("player collision");
+            LightDecayStatusBar lightBar = collision.gameObject.GetComponentInChildren<LightDecayStatusBar>();
+            LightDecay lightArea = collision.gameObject.GetComponentInChildren<LightDecay>();
+            lightBar.GhostContact(2f);
+            lightArea.GhostContactAreaLight(2f);
+        }
+}
+}
