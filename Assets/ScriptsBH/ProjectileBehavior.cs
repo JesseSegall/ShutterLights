6044debd3f016cb5399bbc94f7262ae916dbca74
Tao Quan <83666521+TDQ2@users.noreply.github.com>
2025-04-01 22:06:22 -0400

    boss projectile update

--- Assets/ScriptsBH/ProjectileBehavior.cs
+++ Assets/ScriptsBH/ProjectileBehavior.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class ProjectileBehavior : MonoBehaviour
 {
+
+    public AudioClip launchSound;
+    public AudioClip playerImpactSound;
+    private AudioSource audioSource;
+    void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource == null) {
+            Debug.Log("Projectile Audio is null");
+        }
+        audioSource.PlayOneShot(launchSound);
+    }
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collision");
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Debug.Log("player collision");
+            Debug.Log("player collision");
+            audioSource.PlayOneShot(playerImpactSound);
             LightDecayStatusBar lightBar = collision.gameObject.GetComponentInChildren<LightDecayStatusBar>();
             LightDecay lightArea = collision.gameObject.GetComponentInChildren<LightDecay>();
-            lightBar.DamageTaken(2f);
-            Destroy(gameObject);
+            lightBar.DamageTaken(4f);
+            Invoke("DestroyProjectile", 1.0f);
             //lightArea.GhostContactAreaLight(2f);
         }
         if(collision.gameObject.CompareTag("Terrain")) {
