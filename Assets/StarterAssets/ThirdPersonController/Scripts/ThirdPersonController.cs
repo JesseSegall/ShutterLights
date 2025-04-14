981ab9c3bd6854363595780f7efd4707d9b357fa
Tao Quan <83666521+TDQ2@users.noreply.github.com>
2025-04-14 08:21:48 -0400

    Update jump distance

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -229,6 +229,9 @@ namespace StarterAssets
         {
             // set target speed based on move speed, sprint speed and if sprint is pressed
             float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+            if(!Grounded) {
+                targetSpeed = SprintSpeed;
+            }
 
             // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
